5797725ea72e53ff12746d28d37ac57620d55430
taha <baktash.29.ra@gmail.com>
2020-09-23 08:02:27 +0800

    finalized the assignment

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,78 @@
+require 'json'
+class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  # GET /movies
+  # GET /movies.json
+  def index
+    file = File.read("#{Rails.root}/app/assets/movies.json")
+    data = JSON.parse(file)
+    @movies = data
+    render json: data.to_json
+  end
+
+  # GET /movies/1
+  # GET /movies/1.json
+  def show
+  end
+
+  # GET /movies/new
+  def new
+    @movie = Movie.new
+  end
+
+  # GET /movies/1/edit
+  def edit
+  end
+
+  # POST /movies
+  # POST /movies.json
+  def create
+    @movie = Movie.new(movie_params)
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /movies/1
+  # PATCH/PUT /movies/1.json
+  def update
+    respond_to do |format|
+      if @movie.update(movie_params)
+        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.json { render :show, status: :ok, location: @movie }
+      else
+        format.html { render :edit }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /movies/1
+  # DELETE /movies/1.json
+  def destroy
+    @movie.destroy
+    respond_to do |format|
+      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def movie_params
+      params.require(:movie).permit(:title, :release_date, :poster_path)
+    end
+end
